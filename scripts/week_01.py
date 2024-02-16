9e3703bca2646c1cfed640a7f3e570ee0c0d0188
gasapar <gasapar@seznam.cz>
2024-02-17 00:00:35 +0100

    week 1 finished

--- scripts/week_01.py
+++ scripts/week_01.py
@@ -1,9 +1,25 @@
 import src.simplefuncs as smfc
 
+"""
+*** Poznamka ke cviceni
+* cvicici: Frantisek Gaspar
+* aktivni ucast
+* zapoctova uloha
+* max 3 neomluvene absence
+* cviceni streda a/nebo ctvrtek
+"""
 
 
-#%%
 if __name__ == '__main__':
-    #%%
-    a = smfc.fun(5)
-    print(a)
+    # a = 1
+    # b = -1
+    # c = -2
+    # x1, x2 = smfc.real_quadratic_roots(a, b, c)
+
+    smfc.print_tree(5)
+
+    f_rec = smfc.fib_number_recursion(20)
+    f_cyc = smfc.fib_number_cyclus(20)
+
+    print(f_rec)
+    print(f_cyc)
--- src/simplefuncs.py
+++ src/simplefuncs.py
@@ -1,5 +1,81 @@
-from typing import Any
+import math
 
 
-def fun(x: Any) -> Any:
-    return x
+def real_quadratic_roots(a: float, b: float, c: float) -> tuple[float, float]:
+    """
+    Calculates real roots of quadratic equation: a*x**2 + b*x + c
+    :param a: nonzero quadratic term
+    :param b: linear term
+    :param c: constant term
+    :return: roots of quadratic equation
+    """
+    # check if is quadratic
+    if a == 0.0:
+        raise ValueError
+    # discriminant
+    d = b ** 2.0 - 4.0 * a * c
+    # single root case
+    if d == 0.0:
+        x1 = -0.5 * b / a
+        return x1, x1
+    # complex roots case
+    if d < 0.0:
+        return float('nan'), float('nan')
+    # real root case
+    sqrt_d = math.sqrt(d)
+    x1 = 0.5 * (-b + sqrt_d) / a
+    x2 = 0.5 * (-b - sqrt_d) / a
+    return x1, x2
+
+
+def print_tree(num_levels: int) -> None:
+    """
+    Prints tree into command line.
+    :param num_levels: number of levels of the tree
+    :return: None
+    """
+    if num_levels < 0:
+        raise ValueError
+    if num_levels == 0:
+        return
+
+    for index in range(num_levels):
+        num_spaces = num_levels - index - 1
+        num_stars = 2 * index + 1
+        current_line = ' ' * num_spaces + '*' * num_stars
+        print(current_line)
+
+
+def fib_number_recursion(num: int) -> int:
+    """
+    Recursive implementation of fibonacci numbers.
+    :param num: corresponds to n th fibonacci number
+    :return: n th fibonacci number
+    """
+    if num < 0:
+        raise ValueError
+    if num < 2:
+        return num
+
+    return fib_number_recursion(num - 1) + fib_number_recursion(num - 2)
+
+
+def fib_number_cyclus(num: int) -> int:
+    """
+    Cyclic implementation of fibonacci numbers.
+    :param num: corresponds to n th fibonacci number
+    :return: n th fibonacci number
+    """
+    if num < 0:
+        raise ValueError
+
+    if num < 2:
+        return num
+
+    f_n2 = 0
+    f_n1 = 1
+    for index in range(2, num + 1):
+        f_n = f_n1 + f_n2
+        f_n2 = f_n1
+        f_n1 = f_n
+    return f_n
